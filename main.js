e9e173fe8dc37c9f51e9889f25c3cfa5fbe4fa45
Saiddjon <saidhojiboyev@gmail.com>
2024-10-23 16:13:40 +0500

    change

---
+++ main.js
@@ -0,0 +1,171 @@
+// let img = document.getElementById('img')
+let count = document.getElementById('count')
+let inp = document.getElementById('inp')
+
+fetch(`https://restcountries.com/v3.1/all?fields=name,flags`).then((data)=>{
+
+
+
+return data.json()
+}).then((item)=>{
+    item.array.forEach((counts)=>{
+        
+        count.innerHTML += `<ul class="kolya">
+        <li><img src="${counts.flags.png}"></li>
+        <li>${counts.name.common}</li>
+        </ul>`
+    })
+}).catch((err)=>{
+    console.log(err);
+    
+})
+
+inp.addEventListener('input', ()=>{
+    let val = inp.ariaValueMax.toLocaleLowerCase()
+    const name = document.querySelector('.kolya')
+    name.forEach((item)=>{
+if(item.lastElementChild.textContent.toLocaleLowerCase().includes(val)){
+
+
+}
+
+
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function data(calback){
+const getData = new XMLHttpRequest()
+getData.addEventListener('readystatechange', ()=>{
+
+if(getData.readyState === 4 && getData.status === 200){
+const jsonData = JSON.parse(getData.responseText)
+jsonData.forEach(count => {
+        const counName = count
+        // console.log(counName);
+        calback(counName, undefined)
+    });
+    }else if(getData.readyState === 4){
+    calback(undefined ,'Xato' )
+}
+
+})
+
+getData.open('GET', `https://restcountries.com/v3.1/all?fields=name,flags`)
+getData.send()
+}
+data((data, err)=>{
+    if (err) {
+        console.log(err);
+    }else{
+        count.innerHTML +=`<ul class="kolya">
+        <li><img src="${data.flags.png}"</li>
+        <li>${data.name.common}</li>
+       </ul>`
+    }
+})
+ inp.addEventListener('input', ()=>{
+let val = inp.value.toLowerCase()
+console.log(val);
+
+const name = document.querySelectorAll('.kolya')
+name.forEach((item)=>{
+    if(item.lastElementChild.textContent.toLowerCase().includes(val)){
+        item.classList.remove('hidden')
+    }else{
+        item.classList.add('hidden')
+    }
+})
+
+
+
+
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
